a43b6a1b01a1698010639fe47186eac6178ad83c
wesley <wesley.cursos2024@gmail.com>
2025-10-08 15:29:45 -0300

    index atualizado

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ app.use('/api/contacts', contactsRoutes);
 
 // Inicia o servidor
 app.listen(3000, () => {
-  console.log('Servidor backend rodando em http://localhost:3000');
+  console.log('Servidor backend rodando em http://localhost:3000');
 });
